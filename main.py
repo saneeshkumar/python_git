d2a651224ac72d554b7ad745dae459bdc9eca5c8
saneeshnair <saneeshnair@gmail.com>
2018-02-27 21:50:06 +0900

    Connected to S3

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import data_set
 import time
 import json
+import os
+
+ORDER_DATA_LOCATION = './order_details/'
 
 from boto import kinesis
 
@@ -10,22 +13,28 @@ SHARD_ID = 'shardId-000000000000'
 data_set.create_user_details()
 data_set.create_item_details()
 
+if not os.path.exists(ORDER_DATA_LOCATION):
+    os.makedirs(ORDER_DATA_LOCATION)
+
 kinesis = kinesis.connect_to_region("ap-northeast-1")
 
-records=[];
+records = [];
 
-for order_id in range(0,200):
-	order = data_set.create_order(order_id)
-	order_json=order.create_json()
-	record = {'Data': json.dumps(order_json),'PartitionKey': str(hash(order.user_details.first_name))}
-	records.append(record)
-	if order_id%10 == 0:
-		kinesis.put_records(records, KINESIS_STREAM_NAME)
-		records=[];
+for order_id in range(0, 200):
+    order = data_set.create_order(order_id)
+    order_json = order.create_json()
+    file_name = '{path}order_{order_id}.json'.format(path = ORDER_DATA_LOCATION,order_id=order.id)
+    order_file = open(file_name, "w+")
+    order_file.write(order_json)
+    # record = {'Data': json.dumps(order_json), 'PartitionKey': str(hash(order.user_details.first_name))}
+    # records.append(record)
+    # if order_id % 10 == 0:
+    #     kinesis.put_records(records, KINESIS_STREAM_NAME)
+    #     records = [];
 
-#shard_iter = kinesis.get_shard_iterator(KINESIS_STREAM_NAME, SHARD_ID, "LATEST")["ShardIterator"]
-#while 1==1:
-#	out = kinesis.get_records(shard_iter, limit=2)
-#	shard_iter = out["NextShardIterator"]
-#	print (out)
-#	time.sleep(0.2)
+# shard_iter = kinesis.get_shard_iterator(KINESIS_STREAM_NAME, SHARD_ID, "LATEST")["ShardIterator"]
+# while 1 == 1:
+#     out = kinesis.get_records(shard_iter, limit=2)
+#     shard_iter = out["NextShardIterator"]
+#     print(out)
+#     time.sleep(0.2)
--- order_details.py
+++ order_details.py
@@ -1,62 +1,65 @@
 class OrderStatus:
-	def __init__(self):
-		self.New			= "New"
-		self.Processing		= "Processing"
-		self.Shipped		= "Shipped"
-		self.Delivered		= "Delivered"
-		self.Returned		= "Returned"
-		self.Closed			= "Closed"
+    def __init__(self):
+        self.New = "New"
+        self.Processing = "Processing"
+        self.Shipped = "Shipped"
+        self.Delivered = "Delivered"
+        self.Returned = "Returned"
+        self.Closed = "Closed"
+
 
 class UserDetails:
-	def __init__(self, u_id, first_name, second_name, address):
-		self.id				= u_id
-		self.first_name		= first_name
-		self.second_name	= second_name
-		self.address		= address
+    def __init__(self, u_id, first_name, second_name, address):
+        self.id = u_id
+        self.first_name = first_name
+        self.second_name = second_name
+        self.address = address
 
-	def create_json(self):
-		json_str = "\t\t\"UserDetails\": {\n"
-		json_str += "\t\t\t\"id\": \"" + str(self.id) + "\",\n"
-		json_str += "\t\t\t\"first_name\": \"" + self.first_name + "\",\n"
-		json_str += "\t\t\t\"second_name\": \"" + self.second_name + "\",\n"
-		json_str += "\t\t\t\"address\": \"" + self.address + "\"\n"
-		json_str += "\t\t}"
-		return json_str
+    def create_json(self):
+        json_str = "\t\t\"UserDetails\": {\n"
+        json_str += "\t\t\t\"id\": \"" + str(self.id) + "\",\n"
+        json_str += "\t\t\t\"first_name\": \"" + self.first_name + "\",\n"
+        json_str += "\t\t\t\"second_name\": \"" + self.second_name + "\",\n"
+        json_str += "\t\t\t\"address\": \"" + self.address + "\"\n"
+        json_str += "\t\t}"
+        return json_str
+
 
 class ItemDetails:
-	def __init__(self, item_id, name, color, price):
-		self.id				= item_id
-		self.name			= name
-		self.color			= color
-		self.price			= price
+    def __init__(self, item_id, name, color, price):
+        self.id = item_id
+        self.name = name
+        self.color = color
+        self.price = price
 
-	def create_json(self):
-		json_str = "\t\t\"ItemDetails\": {\n"
-		json_str += "\t\t\t\"id\": \"" + str(self.id) + "\",\n"
-		json_str += "\t\t\t\"name\": \"" + self.name + "\",\n"
-		json_str += "\t\t\t\"color\": \"" + self.color + "\",\n"
-		json_str += "\t\t\t\"price\": \"" + str(self.price) + "\"\n"
-		json_str += "\t\t}"
-		return json_str
+    def create_json(self):
+        json_str = "\t\t\"ItemDetails\": {\n"
+        json_str += "\t\t\t\"id\": \"" + str(self.id) + "\",\n"
+        json_str += "\t\t\t\"name\": \"" + self.name + "\",\n"
+        json_str += "\t\t\t\"color\": \"" + self.color + "\",\n"
+        json_str += "\t\t\t\"price\": \"" + str(self.price) + "\"\n"
+        json_str += "\t\t}"
+        return json_str
+
 
 class OrderDetails:
-	def __init__(self, order_id, user_details, item_details, status, count):
-		self.id				= order_id
-		self.user_details	= user_details
-		self.item_details	= item_details
-		self.status			= status
-		self.count			= count
-		self.cost			= count * item_details.price
+    def __init__(self, order_id, user_details, item_details, status, count):
+        self.id = order_id
+        self.user_details = user_details
+        self.item_details = item_details
+        self.status = status
+        self.count = count
+        self.cost = count * item_details.price
 
-	def create_json(self):
-		json_str = "{\n"
-		json_str += "\t\"OrderDetails\": {\n"
-		json_str += "\t\t\"order_id\": \"" + str(self.id) + "\",\n"
-		json_str += self.user_details.create_json() + ",\n"
-		json_str += self.item_details.create_json() + ",\n"
-		json_str += "\t\t\"status\": \"" + self.status + "\",\n"
-		json_str += "\t\t\"count\": \"" + str(self.count) + "\",\n"
-		json_str += "\t\t\"cost\": \"" + str(self.cost) + "\"\n"
-		json_str += "\t}\n"
-		json_str+="}"
-		return json_str
+    def create_json(self):
+        json_str = "{\n"
+        json_str += "\t\"OrderDetails\": {\n"
+        json_str += "\t\t\"order_id\": \"" + str(self.id) + "\",\n"
+        json_str += self.user_details.create_json() + ",\n"
+        json_str += self.item_details.create_json() + ",\n"
+        json_str += "\t\t\"status\": \"" + self.status + "\",\n"
+        json_str += "\t\t\"count\": \"" + str(self.count) + "\",\n"
+        json_str += "\t\t\"cost\": \"" + str(self.cost) + "\"\n"
+        json_str += "\t}\n"
+        json_str += "}"
+        return json_str
